9558f21fcfd44102c3e77f66ebbe42b7ad8258c8
Cheng-Yeh Chen <gary9192631770@gmail.com>
2018-08-23 17:49:07 +0800

    0823 Keep alive feature complete

--- ArduinoCarArm.py
+++ ArduinoCarArm.py
@@ -3,7 +3,7 @@ import glob
 import atexit
 import serial
 import serial.tools.list_ports
-from pynput import keyboard
+from pynput.keyboard import Key, Controller, Listener
 import threading
 import time
 
@@ -18,7 +18,7 @@ switcher = {
         'LEFT_CMD':    'l',
         'RIGHT_CMD':   'r',
         'STOP_CMD':    's',
-        'BRAKE_CMD':   'v',
+                         'BRAKE_CMD':   'v',
         #Arm
         'BASE_CMD':    'x',
         'SHOULDER_CMD':'u',
@@ -33,7 +33,7 @@ _shoulder_deg = 100
 _elbow_deg = 100
 _gripper_deg = 100
 ser = None
-esc = False
+kill = False
 lock = threading.Lock()
 
 ### method ###
@@ -72,48 +72,61 @@ def get_serial_ports():
             pass
     return result
 
-def connect_routine()
+def get_ports_routine():
+    while input("Scan ports?(y/n):") == 'y':
+        print("Available serial ports:{}".format(get_serial_ports()))
+
+def connect_routine():
     global ser
-    print("Available serial ports:{}".format(get_serial_ports()))
+    
     port = input("Port(tty***/COM***):")
     baud = input("Baudrate:")
 
     try:
         ser = serial.Serial(port, baud, timeout=30)
+        
+    except ValueError:
+        if input("Baudrate should be an integer. Continue?(y/n):") != 'y':
+            access_kill(True)
+            
     except serial.SerialException:
-        print("Connect error")
-        sys.exit(0)
+        if input("Connect error. Continue?(y/n):") != 'y':
+            access_kill(True)
+
 	
 def check_bt():
-    typer = keyboard.Controller()
-    while not read_shared_data("esc"):
-        typer.press(Key.space)
-	time.sleep(1)
-
-def access_esc(write=None):
+    main_thread = threading.main_thread()
+    typer = Controller()
+    
+    while not access_kill():
+        typer.press(Key.esc)
+        time.sleep(1)
+        
+def access_kill(write=None):
+    global kill
     lock.acquire()
     if write:
-	esc = write
-    _return = esc
+        kill = write
+    _return = kill
     lock.release()
     return _return
 
 def on_press(key):
     global _base_deg, _shoulder_deg, _elbow_deg
-    
-    if not ser.is_open():
-	return False
+
+    if not ser or not ser.is_open:
+        return False
     else:
-        elif key == keyboard.Key.up:
+        if key == Key.up:
             cmd_handler('FORWARD_CMD')
             print('Forward')
-        elif key == keyboard.Key.down:
+        elif key == Key.down:
             cmd_handler('BACK_CMD')
             print('Backward')
-        elif key == keyboard.Key.left:
+        elif key == Key.left:
             cmd_handler('LEFT_CMD')
             print('Turn left')
-        elif key == keyboard.Key.right:
+        elif key == Key.right:
             cmd_handler('RIGHT_CMD')
             print('Turn right')
         elif str(key)[1] == '.':
@@ -145,19 +158,20 @@ def on_press(key):
         elif str(key)[1] == 'g':
             _pwm_B = input("Motor B:")
             cmd_handler('MOT_B_SPD', _pwm_B)
+        elif str(key)[1] == 'x':
+            access_kill(True)
+            return False # Stop listener
+            
 
 def on_release(key):
-    if key == keyboard.Key.up:
+    if key == Key.up:
         cmd_handler('STOP_CMD')
-    elif key == keyboard.Key.down:
+    elif key == Key.down:
         cmd_handler('STOP_CMD')
-    elif key == keyboard.Key.left:
+    elif key == Key.left:
         cmd_handler('STOP_CMD')
-    elif key == keyboard.Key.right:
+    elif key == Key.right:
         cmd_handler('STOP_CMD')
-    elif key == keyboard.Key.esc:
-	access_esc(True)
-        return False # Stop listener
 
 #atexit.register(cleanup)
 
@@ -165,17 +179,22 @@ def on_release(key):
 #Main program start here
 #
 def main():
-    global esc
-
     checkthread = threading.Thread(target = check_bt)
+    checkthread.start()
 
-    while not access_esc():
-	connect_routine()
-        with keyboard.Listener(on_press = on_press, on_release = on_release) as listen:
+    while not access_kill():
+        get_ports_routine()
+        connect_routine()
+
+        if access_kill():
+                break
+        
+        with Listener(on_press = on_press, on_release = on_release) as listener:
             listener.join()
-    
+
     checkthread.join()
-    ser.close()
+    if ser:
+        ser.close()
 
 if __name__ == '__main__':
     main()
